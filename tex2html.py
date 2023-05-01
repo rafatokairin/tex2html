b87fe03d003902a511148ececb2128ac455b4055
Rafael Palheta Tokairin <108299374+rafatokairin@users.noreply.github.com>
2023-04-30 22:36:33 -0300

    Update tex2html.py

--- tex2html.py
+++ tex2html.py
@@ -1,22 +1,25 @@
 import os
 import pypandoc
-# pypandoc.download_pandoc()
+from bs4 import BeautifulSoup
 
 def convert_tex_to_html(tex_file_path):
     try:
-        # Converte o arquivo .tex para .html com a opcao --mathjax
+        # Converte o arquivo .tex para .html com a opcao --mathjax e encontra e armazena todos h1 em um vetor de strings
         output = pypandoc.convert_file(tex_file_path, 'html', extra_args=['--mathjax'])
-        return output
+        soup = BeautifulSoup(output, 'html.parser')
+        h1_list = [h1.get_text() for h1 in soup.find_all('h1')]
+        h1_list = [h1.replace(' ', '-') for h1 in h1_list]
+        return str(soup), h1_list
     except Exception as e:
         print(f'Ocorreu um erro durante a conversão: {e}')
-        return None
+        return None, None
 
 # Nome do arquivo .tex de entrada (assumindo que esteja na mesma pasta do arquivo Python)
 tex_file_name = 'main.tex'
 # Caminho do arquivo .tex de entrada
 tex_file_path = os.path.abspath(tex_file_name)
-# Funcao que realiza a conversao
-html_content = convert_tex_to_html(tex_file_path)
+# Chame a função para realizar a conversão
+html_content, h1_list = convert_tex_to_html(tex_file_path)
 
 if html_content is not None:
     with open(f"{os.path.splitext(tex_file_name)[0]}.html", 'w', encoding='utf-8') as f:
@@ -30,6 +33,14 @@ if html_content is not None:
         f.write('<meta charset="utf-8">\n')
         f.write('</head>\n')
         f.write('<body>\n')
+        # Adiciona o índice
+        f.write('<div id="index">\n')
+        f.write('<h2>Outline:</h2>\n')
+        f.write('<ol>\n')
+        for h1 in h1_list:
+            f.write(f'<li><a href="#{h1.lower()}">{h1.replace("-", " ")}</a></li>\n')
+        f.write('</ol>\n')
+        f.write('</div>\n')
         f.write(html_content)
         f.write('</body>\n')
         f.write('</html>')
