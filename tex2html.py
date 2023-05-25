21789710ece8616af5793b5d7e8c56cf17c22bc4
Rafael Palheta Tokairin <108299374+rafatokairin@users.noreply.github.com>
2023-05-25 18:32:31 -0300

    Update tex2html.py

--- tex2html.py
+++ tex2html.py
@@ -20,7 +20,6 @@ def buscar_termos_arquivo_tex(filename):
 
     # Cria um dicionário para armazenar os valores encontrados
     resultados = {}
-
     # Busca pelos termos no arquivo
     for termo in termos:
         match = re.search(termo, conteudo)
@@ -42,45 +41,60 @@ def buscar_termos_arquivo_tex(filename):
 # Nome do arquivo .tex de entrada (assumindo que esteja na mesma pasta do arquivo Python)
 tex_file_name = 'main.tex'
 
-# Lê o arquivo de entrada, remove os asteriscos e adiciona \[
+# Lê o arquivo de entrada, remove os asteriscos
 with open(tex_file_name, "r", encoding="utf-8") as f:
     conteudo = f.read()
     conteudo_sem_asteriscos = re.sub(r"\\(begin|end){([^}\s]+)\*}", r"\\\1{\2}", conteudo)
     # Substituições no conteúdo atualizado do arquivo tex
-    conteudo_atualizado = re.sub(r"\\begin{equation}(\s*\\label{[^}]+})", r"\\begin{equation}\g<1>\[", conteudo_sem_asteriscos)
+    conteudo_atualizado = re.sub(r"\\begin{equation}(\s*\\label{[^}]+})", r"\\begin{equation}\g<1>", conteudo_sem_asteriscos)
     conteudo_atualizado = conteudo_atualizado.replace('\\bm{', '\\mathbf{')
-    conteudo_atualizado = conteudo_atualizado.replace('\\textsuperscript{\\textregistered}', '&reg;')
-    conteudo_atualizado = conteudo_atualizado.replace('\\textregistered', '&reg;')
-    conteudo_atualizado = conteudo_atualizado.replace('\\copyright', '&copy;')
     conteudo_atualizado = conteudo_atualizado.replace('\\hdots', '\\dots')
     conteudo_atualizado = re.sub(r'\\parbox\{.*?\}', '', conteudo_atualizado)
     conteudo_atualizado = conteudo_atualizado.replace('\\centering', '')
+    conteudo_atualizado = conteudo_atualizado.replace(r'\begin{otherlanguage}{brazil}', '')
 
 def convert_tex_to_html(tex_content):
     try:
         # Converte o conteúdo .tex para .html com a opcao --mathjax e encontra e armazena todos h1 em um vetor de strings
         output = pypandoc.convert_text(tex_content, 'html', format='tex', extra_args=['--mathjax'])
         soup = BeautifulSoup(output, 'html.parser')
-        h1_list = [h1.get_text() for h1 in soup.find_all('h1')]
-        h1_list = [h1.lstrip() if h1.startswith(' ') else h1 for h1 in h1_list]
-        h1_list = [h1.rstrip().replace(' ', '-') if h1.endswith(' ') else h1.replace(' ', '-') for h1 in h1_list]
-        return str(soup), h1_list
+        return str(soup)
     except Exception as e:
         print(f'Ocorreu um erro durante a conversão: {e}')
         return None, None
 
-# Define a função para substituir o conteúdo necessário
-def substituir_conteudo(html_content):
-    padrao = r'<span class="math display">\\\[\\label{([^}]+)}'
-    conteudo_com_id = re.sub(padrao, r'<span class="math display" id="\1">', html_content)
+# Define a função para substituir o conteúdo necessário do html
+def substituir_conteudo(conteudo_com_id):
+    conteudo_com_id = conteudo_com_id.replace('\\textsuperscript{\\textregistered}', '&reg;')
+    conteudo_com_id = conteudo_com_id.replace('\\textregistered', '&reg;')
+    conteudo_com_id = conteudo_com_id.replace('\\copyright', '&copy;')
     return conteudo_com_id
 
+def substituir_span(html):
+    pattern = r'\\label\{([\w:]+)\}'
+    matches = re.findall(pattern, html)
+    ids = []
+
+    for label in matches:
+        ids.append(label)
+        html = html.replace(f'\\label{{{label}}}', '')
+    span_pattern = r'<span class="math display">'
+    spans = re.split(span_pattern, html)
+
+    for i, span in enumerate(spans[1:]):
+        if i < len(ids):
+            span_id = f'id="{ids[i]}"'
+            spans[i + 1] = f'<span {span_id} class="math display">{span.strip()}</span>'
+    html = ''.join(spans)
+    return html
+
 # Chame a função para realizar a conversão
-html_content, h1_list = convert_tex_to_html(conteudo_atualizado)
+html_content = convert_tex_to_html(conteudo_atualizado)
 resultados = buscar_termos_arquivo_tex(tex_file_name)
 
 if html_content is not None:
-    conteudo_com_id = substituir_conteudo(html_content)
+    substuido = substituir_conteudo(html_content)
+    conteudo_com_id = substituir_span(substuido)
     new_html_file = f"{os.path.splitext(tex_file_name)[0]}.html"
     with open(new_html_file, 'w', encoding='utf-8') as f:
         html_content = f'''<!DOCTYPE html>
@@ -92,19 +106,11 @@ if html_content is not None:
 </head>
 <body>
     <h1>{list(resultados.values())[0]}</h1>
-    <p>{list(resultados.values())[1]}</p>
-    <p>DOI {list(resultados.values())[2]}</p>
-    <p>Semina: Ciências Exatas e Tecnológicas, Londrina{''.join(list(resultados.values())[3:])}</p>
-    <div id="index">
-        <h2>Outline:</h2>
-        <ol>
-'''
-        for h1 in h1_list:
-            html_content += f'<li><a href="#{h1.lower()}">{h1.replace("-", " ")}</a></li>\n'
-        html_content += '''
-        </ol>
-    </div>
+    <h3>{list(resultados.values())[1]}</h3>
+    <p><strong>DOI</strong> {list(resultados.values())[2]}</p>
+    <p><strong>Citation</strong> Semina: Ciências Exatas e Tecnológicas, Londrina{''.join(list(resultados.values())[3:])}</p>
+    <h3><strong>Abstract:</strong></h3>
     ''' + conteudo_com_id + '''
 </body>
 </html>'''
-        f.write(html_content)
+    f.write(html_content)
